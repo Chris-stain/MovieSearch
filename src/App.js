88c4ea245cb98f828e0d28a4fab808e2f3a7ee9d
Chris-stain <chris_jimenez93@hotmail.com>
2019-08-07 21:37:42 -0700

    Added performSearch function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js'
+import $ from 'jquery'
+import { readSync } from 'fs';
 
 
 class App extends Component {
+
+
   constructor(props) {
     super(props)
-    console.log("This is my initializer")
+    this.state = {}
+    // console.log("This is my initializer")
 
-    const movies = [
-      {id: 0, title:"AvengersL Infinity War", overview:"OMG Thanos" }
-    ]
+    // const movies = [
+    //   {id: 0, poster_src: "https://image.tmdb.org/t/p/w185_and_h278_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title:"Avengers Infinity War", overview:"OMG Thanos" },
+    //   {id: 1, poster_src:"https://image.tmdb.org/t/p/w185_and_h278_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg", title:"Avengers Endgame", overview:"OMG Thanos!!!"}
+    // ]
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
 
 
-    this.state = {rows: [
-      <p key="1"> This Is My Row</p>,
-      <p key="2"> This Is My Row</p>,
-      <p key="3"> This Is My Row</p>
-      
-    ]}
+    // this.state = {rows: movieRows}
+
+    this.performSearch("Ant man")
+  }
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=a0b1480d33829874e8c7e33f24636268&query=" + searchTerm
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        console.log(searchResults)
+        const results = searchResults.results
+        //console.log(results[0])
+
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <MovieRow key ={movie.id} movie = {movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
   }
 
   render() { 
     return (
-      <div className="App">
+      <div>
       
         <table className="titleBar"> 
           <tbody>
@@ -45,7 +90,7 @@ class App extends Component {
           paddingTop: 8,
           paddingBottom: 8,
           paddingLeft: 16
-        }} placeholder="Enter search term"/>
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term"/>
 
         {this.state.rows}
 
